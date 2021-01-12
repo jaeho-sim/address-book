614a31ad5acd3a203168ede6f228ec37b5b2671e
Jaeho Sim <simjh0919@gmail.com>
2021-01-12 12:41:18 -0500

    Add contact list page in the components file, and add router

---
+++ src/components/ContactList/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactList = () => {
+  return (
+    <React.Fragment>
+      <div>This is contact list page</div>
+    </React.Fragment>
+  );
+};
+
+export default ContactList;
