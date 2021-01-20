28440456621c83afee23ed16541540ccee624cb2
Jaeho Sim <simjh0919@gmail.com>
2021-01-20 01:53:22 -0500

    change api result to 20 to have more result

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,4 +1,4 @@
 export const fetchContactData = async () => {
-  const response = await fetch('https://randomuser.me/api/?results=10&seed=fa68f06333af18b7');
+  const response = await fetch('https://randomuser.me/api/?results=20&seed=fa68f06333af18b7');
   return await response.json();
 }
